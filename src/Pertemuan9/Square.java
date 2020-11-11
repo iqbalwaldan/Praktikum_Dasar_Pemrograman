2aebe17767573282856d2e76a2b2bbd11d374ad2
iqbalwaldan <iqbalwaldan0510@gmaail.com>
2020-11-11 09:38:26 +0700

    Materi Praktikum Dasar Pemrograman Semester 1, Pertemuan 9, Square.java

---
+++ src/Pertemuan9/Square.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author iqbal
+ */
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int n = sc.nextInt();
+        for(int iOuter=1; iOuter<=n; iOuter++){
+            for(int i=1; i<=n; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
