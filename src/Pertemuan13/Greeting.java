c1b64f8719b88d070392e3325b00f9d0b4113d55
iqbalwaldan <iqbalwaldan0510@gmaail.com>
2020-12-11 15:49:30 +0700

    Materi Praktikum Dasar Pemrograman Semester 1, Pertemuan 13, Greeting.java

---
+++ src/Pertemuan13/Greeting.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan13;
+
+/**
+ *
+ * @author iqbal
+ */
+class Greeting {
+
+    static void beriSalam() {
+        System.out.println("Halo! Selamat Pagi");
+    }
+
+    public static void main(String[] args) {
+        beriSalam();
+    }
+}
